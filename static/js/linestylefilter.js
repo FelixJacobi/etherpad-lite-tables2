4f508b6258efcc7be5e082be62b55ea3c3af108d
Felix Jacobi <felix.jacobi@stsbl.de>
2017-02-13 04:50:54 +0100

    Transformed package into install wrapper for npm.

--- static/js/linestylefilter.js
+++
@@ -1,8 +0,0 @@
-exports.disableAuthorColorsForThisLine = function (hook, context) {
-    var lineText = context.text;
-    var disableLineColors = false;
-    if (lineText && lineText.indexOf('data-tables')!=-1) {
-       disableLineColors = true;
-    }
-    return disableLineColors;
-};
